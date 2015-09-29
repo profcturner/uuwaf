c721295c2e023018faf540de07f6fe9f3ca5ef3b
Colin Turner <c.turner@ulster.ac.uk>
2015-09-29 21:18:34 +0100

    Version update.

--- include/WA.class.php
+++ include/WA.class.php
@@ -40,7 +40,7 @@ require_once('Log.php');
 *
 * @author Colin Turner <c.turner@ulster.ac.uk>
 * @author Gordon Crawford <g.crawford@ulster.ac.uk>
-* @version 2.0
+* @version 2.1
 * @package UUWAF
 * @see UUWAF.class.php
 *
